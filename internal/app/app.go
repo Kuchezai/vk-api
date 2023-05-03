894794202370151ed10ff0d287c5b152d1530f22
Kuchezai <Kozhenov.A.A@yandex.ru>
2023-05-03 13:11:00 +0300

    feat: create entrypoint

---
+++ internal/app/app.go
@@ -0,0 +1,31 @@
+package app
+
+import (
+	"log"
+
+	"github.com/gin-gonic/gin"
+
+	"vk-api/config"
+	"vk-api/internal/repo/webapi"
+	"vk-api/internal/transport/http"
+	v1 "vk-api/internal/transport/http/v1"
+	"vk-api/internal/usecase"
+	"vk-api/pkg/logger"
+)
+
+func Run(cfg *config.Config) {
+
+	wa := webapi.NewUserWebAPI(cfg.VK.KeyAPI)
+	userUsecase := usecase.NewUserUsecase(wa)
+
+	logger, err := logger.New("logs.txt")
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+
+	userHandler := v1.NewUserHandler(userUsecase, logger)
+
+	router := gin.New()
+	http.StartNewServer(router, logger, userHandler)
+
+}
